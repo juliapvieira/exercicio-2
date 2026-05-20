ac2a310ef9886c922c2137995eeac22391cd318f
julia <julia.pardim@icloud.com>
2026-05-20 11:31:23 -0300

    exercicio3

---
+++ exercicio3.py/exercicio3.py
@@ -0,0 +1,88 @@
+# EX1
+# Calcule o total de alunos em duas turmas:
+# uma com 28 alunos e outra com 32.
+turma1 = 28
+turma2 = 32
+resultado = turma1 + turma2
+print (resultado)
+
+# EX2
+# Um pacote de dados tem 500MB.
+# Após baixar 150MB, quanto resta?
+dados1 = 500
+dados2 = 150
+resultado = dados1 - dados2
+print (resultado)
+
+
+# EX3
+# Uma caixa contém 24 ovos.
+# Quantos ovos existem em 8 caixas?
+caixa = 24
+resultado = 24*8
+print (resultado)
+
+
+# EX4
+# Divida 10 litros de suco igualmente
+# entre 3 pessoas e mostre o resultado.
+suco = 10
+pessoas = 3
+resultado = 10/3
+print (resultado)
+
+# EX5
+# Use divisão inteira para descobrir
+# quantas pessoas recebem 3 litros inteiros
+# dos 10 litros disponíveis.
+suco = 10
+pessoas = 3
+resultado = 10//3
+print (resultado)
+#duas barras (//) porque é uma divisão inteira
+
+# EX6
+# Calcule o resto da divisão de 10 por 3.
+suco = 10
+pessoas = 3
+resultado = 10%3
+print (resultado)
+#% pois é o resto
+
+# EX7
+# Calcule 2 elevado à potência 8.
+#potencia: **
+num1 = 2
+num2 = 8
+resultado = num1 ** num2
+print (resultado)
+
+# EX8
+# Calcule a raiz quadrada de 81
+# usando exponenciação.
+#para calcular raíz quadrada ** (1/2)
+num1 = 81
+resultado = num1 ** (1/2) 
+print (resultado)
+
+# EX9
+# Calcule a expressão:
+# (10 + 5) * 2 - 8 / 4
+num1 = 10
+num2 = 5
+num3 = 2
+num4 = 8
+num5 = 4
+resultado = (num1 + num2) * num3 - num4 / num5
+print (resultado)
+
+# EX10
+# Mostre o resultado da divisão de -7 por 2
+# usando divisão inteira e o operador de resto.
+num1 = -7
+num2 = 2
+resultado = num1 // num2
+print (resultado) 
+resto = num1 % num2
+print (resto)
+
